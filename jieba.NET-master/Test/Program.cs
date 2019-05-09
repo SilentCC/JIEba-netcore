31d8ac6165065f13cb9fae255447d1fa6a6e345d
小康 <xiaokang@cnblogs.com>
2019-05-09 14:06:56 +0800

    add web and api to dict for special tokenize

--- jieba.NET-master/Test/Program.cs
+++ jieba.NET-master/Test/Program.cs
@@ -12,19 +12,19 @@ namespace Test
         {
            
             var segmenter = new JiebaSegmenter();
-            var segments = segmenter.Cut("我来到北京清华大学", cutAll: true);
+            var segments = segmenter.Cut("WebApi 教程", cutAll: true);
             Console.WriteLine("【全模式】：{0}", string.Join("/ ", segments));
 
-            segments = segmenter.Cut("我来到北京清华大学");  // 默认为精确模式
+            segments = segmenter.Cut("WebApi 教程");  // 默认为精确模式
             Console.WriteLine("【精确模式】：{0}", string.Join("/ ", segments));
 
-            segments = segmenter.Cut("他来到了网易杭研大厦");  // 默认为精确模式，同时也使用HMM模型
+            segments = segmenter.Cut("WebApi 教程");  // 默认为精确模式，同时也使用HMM模型
             Console.WriteLine("【新词识别】：{0}", string.Join("/ ", segments));
 
-            segments = segmenter.CutForSearch("小明硕士毕业于中国科学院计算所，后在日本京都大学深造"); // 搜索引擎模式
+            segments = segmenter.CutForSearch("webapi 教程"); // 搜索引擎模式
             Console.WriteLine("【搜索引擎模式】：{0}", string.Join("/ ", segments));
 
-            segments = segmenter.Cut("结过婚的和尚未结过婚的");
+            segments = segmenter.Cut("webapi 教程");
             Console.WriteLine("【歧义消除】：{0}", string.Join("/ ", segments));
             Console.ReadKey();
         }
